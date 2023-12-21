07a460217e46beffe2a39b34ab953e57b2e5f847
DESKTOP-2QJHBCI\Administrator <1402357376@qq.com>
2023-12-21 22:10:57 +0800

    成功运行环境

--- sky-server/src/main/java/com/sky/config/MinioConfiguration.java
+++ sky-server/src/main/java/com/sky/config/MinioConfiguration.java
@@ -1,21 +1,21 @@
-package com.sky.config;
-
-import com.sky.properties.MinioProperties;
-import io.minio.MinioClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MinioConfiguration {
-
-    @Autowired
-    private MinioProperties minioProp;
-
-    @Bean
-    public MinioClient minioClient(){
-        MinioClient client = new MinioClient(minioProp.getEndpoint(), minioProp.getAccesskey(),minioProp.getSecretkey());
-        return client;
-    }
-
-}
+//package com.sky.config;
+//
+//import com.sky.properties.MinioProperties;
+//import io.minio.MinioClient;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+////@Configuration
+//public class MinioConfiguration {
+//
+//    @Autowired
+//    private MinioProperties minioProp;
+//
+//    @Bean
+//    public MinioClient minioClient(){
+//        MinioClient client = new MinioClient(minioProp.getEndpoint(), minioProp.getAccesskey(),minioProp.getSecretkey());
+//        return client;
+//    }
+//
+//}
